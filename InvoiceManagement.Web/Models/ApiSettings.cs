0bdf44272718b8f29e66ea1ebe37640ab9937e80
Sithmi Himanshi <sithmihimanshi@icloud.com>
2026-08-02 01:09:58 +0530

    Configure MVC project and session management for frontend

---
+++ InvoiceManagement.Web/Models/ApiSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace InvoiceManagement.Web.Models;
+
+public class ApiSettings
+{
+    public string BaseUrl { get; set; } = string.Empty;
+}
--- InvoiceManagement.Web/Program.cs
+++ InvoiceManagement.Web/Program.cs
@@ -1,15 +1,30 @@
+using InvoiceManagement.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.Configure<ApiSettings>(
+    builder.Configuration.GetSection("ApiSettings"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,6 +32,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
